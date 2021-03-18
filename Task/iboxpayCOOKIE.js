f3669793ac561039875eae28fd3cd58513d5ba65
jrqzhx <64943263+jrqzhx@users.noreply.github.com>
2021-03-18 10:59:09 +0800

    Add files via upload

---
+++ Task/iboxpayCOOKIE.js
@@ -0,0 +1,21 @@
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行
+
+let refreshtokenVal= ``
+let iboxpayvideoheaderVal= ``
+let iboxpayvideobodyVal= ``
+
+
+let iboxpaycookie = {
+
+  refreshtokenVal: refreshtokenVal, 
+    iboxpayvideoheaderVal: iboxpayvideoheaderVal,  
+      iboxpayvideobodyVal: iboxpayvideobodyVal,   
+  
+}
+
+module.exports =  iboxpaycookie
+  
+
+
